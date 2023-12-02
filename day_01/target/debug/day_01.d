459213fe6d63659770bb4148c0e9f0b7af0f0734
Daniel Zenke <danielzenke94@gmail.com>
2023-12-02 13:49:54 +0100

    cleanup ignored files

--- day_01/target/debug/day_01.d
+++
@@ -1,1 +0,0 @@
-/home/stan/Code/advent_of_code/2023/day_01/target/debug/day_01: /home/stan/Code/advent_of_code/2023/day_01/src/main.rs
--- day_01/target/debug/deps/day_01-23c6a727d7f5e211.d
+++
@@ -1,5 +0,0 @@
-/home/stan/Code/advent_of_code/2023/day_01/target/debug/deps/day_01-23c6a727d7f5e211.rmeta: src/main.rs
-
-/home/stan/Code/advent_of_code/2023/day_01/target/debug/deps/day_01-23c6a727d7f5e211.d: src/main.rs
-
-src/main.rs:
