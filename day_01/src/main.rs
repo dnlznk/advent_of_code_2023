b404b66a6a58302e9c1e2e71b57db558de6858ff
Daniel Zenke <danielzenke94@gmail.com>
2023-12-02 15:07:11 +0100

    finished day 1

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -2,8 +2,12 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    let path = Path::new("input.txt");
-    let input = fs::read_to_string(path).unwrap();
+    part_one();
+    part_two();
+}
+
+fn part_one() {
+    let input = read_file();
     let sum = input.lines().map(|line| {
         let first = line.chars().find(|c| c.is_numeric()).unwrap();
         let last = line.chars().rev().find(|c| c.is_numeric()).unwrap();
@@ -12,3 +16,26 @@ fn main() {
     }).sum::<u32>();
     println!("Result is {sum}");
 }
+
+fn part_two() {
+    let digits = vec![("one", "o1e"), ("two", "t2o"), ("three", "t3e"), ("four", "f4r"), ("five", "f5e"), ("six", "s6x"), ("seven", "s7n"), ("eight", "e8t"), ("nine", "n9e")];
+    let input = read_file();
+    let sum: u32 = input.lines().map(|line| {
+        let mut line = line.to_string();
+        digits.iter().for_each(|digit| {
+            line = line.replace(digit.0, &digit.1);
+            ()
+        });
+        let first = line.chars().find(|c| c.is_numeric()).unwrap();
+        let last = line.chars().rev().find(|c| c.is_numeric()).unwrap();
+        let number = format!("{first}{last}");
+        number.parse::<u32>().unwrap()
+    }).sum();
+    println!("Result is {sum}");
+}
+
+fn read_file() -> String {
+    let path = Path::new("input.txt");
+    fs::read_to_string(path).unwrap()
+}
+
