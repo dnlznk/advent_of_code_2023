36dd09c8567475a4b6c4d736c48fa95179e949d1
Daniel Zenke <danielzenke94@gmail.com>
2023-12-03 12:42:25 +0100

    adjust day 01 code

--- day_01/src/main.rs
+++ day_01/src/main.rs
@@ -2,36 +2,12 @@ use std::fs;
 use std::path::Path;
 
 fn main() {
-    part_one();
-    part_two();
-}
-
-fn part_one() {
     let input = read_file();
-    let sum = input.lines().map(|line| {
-        let first = line.chars().find(|c| c.is_numeric()).unwrap();
-        let last = line.chars().rev().find(|c| c.is_numeric()).unwrap();
-        let number = format!("{first}{last}");
-        number.parse::<u32>().unwrap()
-    }).sum::<u32>();
-    println!("Result is {sum}");
-}
+    let part_one = sum_lines(input.clone());
+    let part_two = sum_lines(parse_digits(input));
 
-fn part_two() {
-    let digits = vec![("one", "o1e"), ("two", "t2o"), ("three", "t3e"), ("four", "f4r"), ("five", "f5e"), ("six", "s6x"), ("seven", "s7n"), ("eight", "e8t"), ("nine", "n9e")];
-    let input = read_file();
-    let sum: u32 = input.lines().map(|line| {
-        let mut line = line.to_string();
-        digits.iter().for_each(|digit| {
-            line = line.replace(digit.0, &digit.1);
-            ()
-        });
-        let first = line.chars().find(|c| c.is_numeric()).unwrap();
-        let last = line.chars().rev().find(|c| c.is_numeric()).unwrap();
-        let number = format!("{first}{last}");
-        number.parse::<u32>().unwrap()
-    }).sum();
-    println!("Result is {sum}");
+    println!("Result part one: {part_one}");
+    println!("Result part two: {part_two}");
 }
 
 fn read_file() -> String {
@@ -39,3 +15,32 @@ fn read_file() -> String {
     fs::read_to_string(path).unwrap()
 }
 
+fn sum_lines(input: String) -> usize {
+    input
+        .lines()
+        .map(|line| {
+            let is_numeric = |c: &char| c.is_numeric();
+            let first = line.chars().find(is_numeric).unwrap();
+            let last = line.chars().rev().find(is_numeric).unwrap();
+            format!("{first}{last}").parse::<usize>().unwrap()
+        })
+        .sum()
+}
+
+fn parse_digits(input: String) -> String {
+    input
+        .lines()
+        .map(|line| {
+            line.replace("one", "o1e")
+                .replace("two", "t2o")
+                .replace("three", "t3e")
+                .replace("four", "f4r")
+                .replace("five", "f5e")
+                .replace("six", "s6x")
+                .replace("seven", "s7n")
+                .replace("eight", "e8t")
+                .replace("nine", "n9e")
+        })
+        .collect::<Vec<String>>()
+        .join("\n")
+}
