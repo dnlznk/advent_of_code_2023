e1f11229ec5df36c3245a3d8d466a97f1dcf3e3c
Daniel Zenke <danielzenke94@gmail.com>
2023-12-03 14:56:16 +0100

    finished day 02

--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -4,9 +4,11 @@ const MAX_BLUE: usize = 14;
 
 fn main() {
     let input = read_file();
-    let part_one = sum_valid_game_ids(input);
+    let part_one = sum_valid_game_ids(input.clone());
+    let part_two = minimum_number_of_cubes(input);
 
     println!("Result part one: {part_one}");
+    println!("Result part two: {part_two}");
 }
 
 fn read_file() -> String {
@@ -33,11 +35,13 @@ fn sum_valid_game_ids(input: String) -> usize {
                         .collect::<Vec<String>>()
                 })
                 .map(|set| {
-                    set
-                        .iter()
+                    set.iter()
                         .map(|value| {
                             let value: Vec<&str> = value.split(" ").collect();
-                            let (quantity, color) = (value.first().unwrap().parse::<usize>().unwrap(), value.last().unwrap().to_string());
+                            let (quantity, color) = (
+                                value.first().unwrap().parse::<usize>().unwrap(),
+                                value.last().unwrap().to_string(),
+                            );
 
                             match color.as_str() {
                                 "red" => quantity <= MAX_RED,
@@ -46,14 +50,10 @@ fn sum_valid_game_ids(input: String) -> usize {
                                 _ => panic!(),
                             }
                         })
-                        .reduce(|acc, value| {
-                            !(!acc || !value)
-                        })
+                        .reduce(|acc, value| !(!acc || !value))
                         .unwrap()
                 })
-                .reduce(|acc, value| {
-                    !(!acc || !value)
-                })
+                .reduce(|acc, value| !(!acc || !value))
                 .unwrap();
 
             if valid_sets {
@@ -62,5 +62,42 @@ fn sum_valid_game_ids(input: String) -> usize {
                 None
             }
         })
-        .sum::<usize>()
+        .sum()
+}
+
+fn minimum_number_of_cubes(input: String) -> usize {
+    input
+        .lines()
+        .map(|line| line.split(": ").last().unwrap())
+        .map(|line| {
+            let mut red: usize = 0;
+            let mut green: usize = 0;
+            let mut blue: usize = 0;
+
+            line
+                .split("; ")
+                .map(|set| {
+                    set.split(", ").for_each(|values| {
+                        let value: Vec<&str> = values.split(" ").collect();
+                        let (quantity, color) = (
+                            value.first().unwrap().parse::<usize>().unwrap(),
+                            value.last().unwrap().to_string(),
+                        );
+
+                        match color.as_str() {
+                            "red" => red = red.max(quantity),
+                            "green" => green = green.max(quantity),
+                            "blue" => blue = blue.max(quantity),
+                            _ => panic!(),
+                        }
+                    });
+
+
+                    (red * green * blue) as usize
+                })
+                .for_each(drop);
+
+            (red * green * blue) as usize
+        })
+        .sum()
 }
